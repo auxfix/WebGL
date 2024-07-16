c82cbb6ad938affcdf877dc344e9f1389c8000f3
oleg shchepotkin <music.wave.form7@gmail.com>
2024-07-17 00:03:15 +0300

    (render): first render

--- main.js
+++ main.js
@@ -14,8 +14,6 @@ let fragmentShaderText =
 [
 'precision mediump float;',
 '',
-'attribute vec2 vertPosition;',
-'',
 'void main()',
 '{',
 ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
@@ -46,8 +44,61 @@ window.initDemo = function () {
 
   gl.compileShader(vertexShader);
   if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
-    console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader))
+    console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+    return;
   }
 
   gl.compileShader(fragmentShader);
+  if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+    console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+    return;
+  }
+
+  let program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
+    console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+    return;
+  }
+
+  gl.validateProgram(program);
+  if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS)){
+    console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+    return;
+  }
+
+  // 
+  // Create buffer
+  //
+  let triangleVerticies = [
+    // X, Y
+    0.0, 0.5,
+    -0.5, -0.5,
+    0.5, -0.5
+  ]
+
+  let triangleVertexBufferObject = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVerticies), gl.STATIC_DRAW);
+
+  let positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+
+  gl.vertexAttribPointer(
+    positionAttribLocation, // Attribute location
+    2, // Number of elements per attribute
+    gl.FLOAT, // Type of elements
+    gl.FALSE,
+    2 * Float32Array.BYTES_PER_ELEMENT, // size of individual vertex
+    0 // Offset from the beggining of a single vertext to this attribute
+  );
+
+  gl.enableVertexAttribArray(positionAttribLocation);
+
+  //
+  // Main loop
+  //
+  gl.useProgram(program);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
