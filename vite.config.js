b6d984633ad49c8b994dbd37e03388f57414d1f3
oleg shchepotkin <music.wave.form7@gmail.com>
2024-07-18 23:33:51 +0300

    (fix): build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  build: {}
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        triangle: resolve(__dirname, 'lib/scenes/triangle/page.html')
+      }
+    }
+  }
 })
