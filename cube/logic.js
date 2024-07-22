619a9f428b5c4d504ea6c0213c4495fe2e638599
oleg shchepotkin <music.wave.form7@gmail.com>
2024-07-22 21:14:26 +0300

    (cube): indices

--- cube/logic.js
+++ cube/logic.js
@@ -119,7 +119,33 @@ window.initCubeScene = function () {
     -1.0, -1.0, 1.0,      0.5, 0.5, 0.1,
     1.0, -1.0, 1.0,       0.5, 0.5, 0.1,
     1.0, 1.0, 1.0,        0.5, 0.5, 0.1,
-  ]
+  ];
+
+  let cubeIndices = [
+    // Top
+    0, 1, 2,
+    0, 2, 3,
+
+    // Left
+    5, 4, 6,
+    6, 4, 7,
+
+    // Right
+    8, 9, 10,
+    8, 10, 11,
+
+    // Front
+    13, 12, 14,
+    15, 14, 12,
+
+    // Back
+    16, 17, 18,
+    16, 18, 19,
+
+    // Bottom
+    21, 20, 22,
+    22, 20, 23,
+  ];
 
   let triangleVertexBufferObject = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
